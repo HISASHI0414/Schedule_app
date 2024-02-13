f81a0330d4da44e7c1f964e82cd88608507423e1
HISASHI FUJISATO <0414.hisashi.f@gmail.com>
2024-02-13 19:09:35 +0900

    Mailer機能の実装 新規スケジュール登録時にタイトル名がユーザーメアドからユーザーメアド宛（自分宛）に送信される

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -3,9 +3,17 @@ class SchedulesController < ApplicationController
   end
 
   def new
+    @schedule = current_user.schedules
   end
 
   def create
+    @schedule = current_user.schedules(schedule_params)
+    if @schedule.save
+      ScheduleMailer.creation_schedule_email(@schedule).deliver_now
+      redirect_to root_path, notice: "新規スケジュールを登録しました。"
+    else
+      render :new, notice: "スケジュールの作成に失敗しました。"
+    end
   end
 
   def show
@@ -18,5 +26,10 @@ class SchedulesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def schedule_params
+    params.require(:schedule).permit(params[:title, :started_at, :finished_at, :alarm, :url, :schedule_repeated])
   end
 end
