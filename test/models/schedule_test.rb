456c37fdfb52c9d7e49c068704b9a15049a547fd
HISASHI FUJISATO <0414.hisashi.f@gmail.com>
2024-02-14 15:43:38 +0900

    バッチ処理（開始設定の１５分前に通知するアラーム機能）を実装するために、便宜的に開始日・終了日をdateかdatetimeへ変更

--- test/models/schedule_test.rb
+++ test/models/schedule_test.rb
@@ -4,9 +4,9 @@
 #
 #  id                :bigint           not null, primary key
 #  alarm             :boolean          default(FALSE), not null
-#  end_time          :date             not null
+#  end_time          :datetime         not null
 #  schedule_repeated :boolean          default(FALSE), not null
-#  start_time        :date             not null
+#  start_time        :datetime         not null
 #  title             :string           not null
 #  url               :text
 #  created_at        :datetime         not null
